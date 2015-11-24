f472dee74af70d3c5e5054b6d1602667697ca2ab
Dawson Conway <dconman02@gmail.com>
2015-11-23 20:17:10 -0600

    File System Major Changes
    The filesystem is starting to take shape, and compiles, so I'm
    commiting it. Deleting is flushed out as well as as API.

--- src/FileSystem/FileManager.cpp
+++ src/FileSystem/FileManager.cpp
@@ -1,65 +1,213 @@
 
+#include <cerrno>
+#include <functional>
 #include <iostream>
 #include "FileManager.hpp"
-using namespace std::experimental::filesystem;
+namespace fs = std::experimental::filesystem;
 
 FileManager* FileManager::currentInstance = nullptr;
 
 FileManager::FileManager()
 {
-	currentInstance = this;
+    currentInstance = this;
+    
 }
 
 FileManager::~FileManager()
-{
-	
+{  
 }
 
 FileManager* FileManager::getInstance()
 {
-	if( currentInstance == nullptr )
-	{
-		currentInstance = new FileManager();
-	}
-	
-	return currentInstance;
+    if( currentInstance == nullptr )
+    {
+        currentInstance = new FileManager();
+    }
+    
+    return currentInstance;
 }
 
-void FileManager::checkError( std::error_code& ec )
+t_id FileManager::FileManager::deleteFiles( v_paths& files )
 {
-	std::cerr << ec.value() << std::endl;
-	std::cerr << ec.default_error_condition().value() << std::endl;
-	
+    std::thread t = std::thread( &static_DeleteFiles, files );
+    return t.get_id();
+}
+t_id FileManager::FileManager::deleteFile( fs::path& file )
+{
+    v_paths v = v_paths(1, file);
+    return deleteFiles( v );        
 }
 
-void FileManager::deleteFile( path& file )
+t_id FileManager::copyFiles( v_paths& from
+             , v_paths& to
+             , fs::copy_options options )
 {
-	std::error_code ec;
-	bool success = remove( file, ec );
-	
-	if( !success )
-	{
-		checkError( ec );
-	}
+    std::thread t = std::thread( &static_CopyFiles, from, to, options );
+    return t.get_id();
+}
+t_id FileManager::copyFiles( v_paths& from
+             , fs::path &to
+             , fs::copy_options options )
+{
+    v_paths v = v_paths(1, to);
+    return copyFiles( from, v, options );
+}
+t_id FileManager::copyFile( fs::path &from
+             , fs::path &to
+             , fs::copy_options options )
+{
+    v_paths v1 = v_paths( 1, from );
+    v_paths v2 = v_paths( 1, to );
+    return copyFiles( v1, v2, options );
 }
 
-void FileManager::copyFile( path& from, path& to, copy_options options )
+t_id FileManager::moveFiles( v_paths& from
+             , v_paths& to
+             , fs::copy_options options )
 {
-	std::error_code ec;
-	bool success = copy( from, to, options, ec );
-	
-	if( !success )
-	{
-		checkError( ec );
-	}
+    std::thread t = std::thread( &static_MoveFiles, from, to, options );
+    return t.get_id();
 }
-void FileManager::moveFile( path& from, path& to )
+t_id FileManager::moveFiles( v_paths& from
+             , fs::path &to
+             , fs::copy_options options )
 {
-	std::error_code ec;
-	bool success = rename( from, to, ec );
-	
-	if( !success )
-	{
-		checkError( ec );
+    v_paths v = v_paths(1, to);
+    return moveFiles( from, v, options );
+}
+t_id FileManager::moveFile( fs::path &from
+             , fs::path &to
+             , fs::copy_options options )
+{
+    v_paths v1 = v_paths( 1, from );
+    v_paths v2 = v_paths( 1, to );
+    return moveFiles( v1, v2, options );
+}
+
+void FileManager::static_DeleteFiles( v_paths files )
+{
+	getInstance()->doDeleteFiles( files );
+}
+
+
+void FileManager::static_CopyFiles( v_paths from, v_paths to, fs::copy_options options )
+{
+	getInstance()->doCopyFiles( from, to, options );
+}
+
+void FileManager::static_MoveFiles( v_paths from, v_paths to, fs::copy_options options )
+{
+	getInstance()->doMoveFiles( from, to, options );
+}
+
+void FileManager::doDeleteFiles( v_paths files )
+{
+    std::error_code ec;
+    v_paths canonicals;
+    canonicals.reserve( files.size() );
+    for( v_paths::iterator it = files.begin(); files.end() != it; it++ )
+    {
+		fs::path can = fs::canonical( *it, ec );
+		
+		switch( checkError( ec, *it ) )
+		{
+			case TERMINATE:
+				return;
+				break;
+			case RETRY:
+				it--;
+			/* break intentionally omitted */
+			case IGNORE:
+				continue;
+				break;
+			case NO_ERROR:
+				canonicals.emplace_back( can );
+				break;
+		}
+		
 	}
+		v_paths deleteStack;
+	    deleteStack.reserve( canonicals.size() );
+	
+	while( canonicals.size() > 0 )
+	{
+		std::unique_lock<std::mutex> lock(setLock);
+		notifier.wait( lock, (std::function<bool()>)
+			[&]()
+			{
+				for( v_paths::reverse_iterator it = canonicals.rbegin(); canonicals.rend() != it; it++ )
+				{
+					if( !lockedPaths.count( *it ) )
+					{
+						lockedPaths.insert( *it );
+						deleteStack.emplace_back( *it );
+						canonicals.erase( (it+1).base() );
+					}
+				}
+				return deleteStack.size() == 0 && canonicals.size() > 0;
+			}
+		);
+		lock.unlock();
+		for( v_paths::iterator it = deleteStack.begin(); deleteStack.end() != it; it++ )
+		{
+			fs::remove( *it, ec );
+			switch( checkError( ec, *it ) )
+			{
+				case TERMINATE:
+					lock.lock();
+					for( /*intentionally ommited*/ ; deleteStack.end() != it; it++ )
+					{
+						lockedPaths.erase( *it );
+					}
+					canonicals.clear();
+					notifier.notify_all();
+					lock.unlock();
+					return;
+					break;
+				case RETRY:
+					it--;
+				/* break intentionally omitted */
+				case IGNORE:
+					continue;
+					break;
+				case NO_ERROR:
+					lock.lock();
+					lockedPaths.erase( *it );
+					notifier.notify_all();
+					lock.unlock();
+					break;
+			}
+		}
+	}
+	
+	// TODO notify of success
+
+}
+
+void FileManager::doCopyFiles( v_paths from, v_paths to, fs::copy_options options )
+{
+    std::error_code ec;
+    //fs::copy( from, to, options, ec );
+    
+    if( !ec )
+    {
+        //checkError( ec );
+    }
+}
+void FileManager::doMoveFiles( v_paths from, v_paths to, fs::copy_options options )
+{
+    std::error_code ec;
+    //fs::rename( from, to, ec );
+    
+    if( !ec )
+    {
+        //checkError( ec );
+    }
+}
+
+
+FileManager::HandleErrorCommand FileManager::checkError( std::error_code& ec, fs::path p1, fs::path p2 )
+{
+    std::cerr << ec.message() << std::endl;
+    return IGNORE;
 }
--- src/FileSystem/FileManager.hpp
+++ src/FileSystem/FileManager.hpp
@@ -1,39 +1,102 @@
 
+#include <cassert>
+#include <condition_variable>
 #include <experimental/filesystem>
-using namespace std::experimental::filesystem;
-#include <system_error>
-#include <unordered_map>
+#include <functional>
 #include <mutex>
+#include <system_error>
+#include <thread>
+#include <unordered_set>
+
+namespace fs = std::experimental::filesystem;
 
 #ifndef FILEMANAGER_HPP
 #define FILEMANAGER_HPP
+
+typedef std::thread::id t_id;
+typedef std::vector<fs::path> v_paths;
+
+namespace std
+{
+    template <>
+    struct hash<fs::path>
+    {
+        size_t operator()(const fs::path& key) const
+        {
+            assert( key.is_absolute() );
+            size_t ret = 5381;
+            const fs::path::value_type* str = key.c_str();
+            size_t c;
+            while (c = *str++)
+            {
+                ret = ((ret << 5) + ret) + c; /* hash * 33 + c */
+            }
+            return ret;
+        }
+    };
+}
+
+
 class FileManager
 {
-	public:
-	
-	static FileManager* getInstance();
-	
-	void deleteFile( path& file );
-	void copyFile( path& from
-	             , path& to
-	             , copy_options options = copy_options::none );
-	void moveFile( path& from, path& to );
-	
-	
-	
-	private:
-	
-	FileManager();
-	~FileManager();
-	
-	static FileManager* currentInstance;
-	
-    std::unordered_map<path, void> locks;
-	std::mutex mapLock;
-	
-	void checkError( std::error_code& ec );
-	
-	
+    public:
+    
+    static FileManager* getInstance();
+
+    t_id deleteFile( fs::path &file );
+    t_id deleteFiles( v_paths& file );
+    t_id copyFiles( v_paths& from
+                 , v_paths& to
+                 , fs::copy_options options = fs::copy_options::none );
+    t_id copyFiles( v_paths& from
+                 , fs::path &to
+                 , fs::copy_options options = fs::copy_options::none );
+    t_id copyFile( fs::path &from
+                 , fs::path &to
+                 , fs::copy_options options = fs::copy_options::none );
+    t_id moveFiles( v_paths& from
+                 , v_paths& to
+                 , fs::copy_options options = fs::copy_options::none );
+    t_id moveFiles( v_paths& from
+                 , fs::path &to
+                 , fs::copy_options options = fs::copy_options::none );
+    t_id moveFile( fs::path &from
+                 , fs::path &to
+                 , fs::copy_options options = fs::copy_options::none );
+    
+    enum HandleErrorCommand
+    {
+		TERMINATE,
+		IGNORE,
+		RETRY,
+		NO_ERROR
+	};
+    
+    
+    
+    private:
+    
+    FileManager();
+    ~FileManager();
+    
+    static FileManager* currentInstance;
+    
+
+    static void static_DeleteFiles( v_paths file );
+    static void static_CopyFiles( v_paths from, v_paths to, fs::copy_options options );
+    static void static_MoveFiles( v_paths from, v_paths to, fs::copy_options options );
+    
+	void doDeleteFiles( v_paths file );
+    void doCopyFiles( v_paths from, v_paths to, fs::copy_options options );
+    void doMoveFiles( v_paths from, v_paths to, fs::copy_options options );
+    
+    std::unordered_set<fs::path> lockedPaths;
+    std::mutex setLock;
+    std::condition_variable notifier;
+    
+    HandleErrorCommand checkError( std::error_code& ec, fs::path p1 = fs::path(), fs::path p2 = fs::path() );
+    
+    
 };
 
 

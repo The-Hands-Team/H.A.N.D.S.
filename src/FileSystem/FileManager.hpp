a3dfe93a0b26764765fc3761aea473fef1e709d6
dconman <dconman02@gmail.com>
2016-03-03 13:35:39 -0600

    Still more Changes

--- src/FileSystem/FileManager.hpp
+++ src/FileSystem/FileManager.hpp
@@ -13,8 +13,11 @@ namespace fs = std::experimental::filesystem;
 #ifndef FILEMANAGER_HPP
 #define FILEMANAGER_HPP
 
-typedef std::thread::id t_id;
-typedef std::vector<fs::path> v_paths;
+using t_id = std::thread::id;
+namespace fs
+{
+    using paths = std::vector<fs::path>;
+}
 
 namespace std
 {
@@ -57,20 +60,20 @@ class FileManager
     static FileManager* getInstance();
 
     t_id deleteFile( const fs::path &file );
-    t_id deleteFiles( v_paths& file, fs::copy_options options = fs::copy_options::none  );
-    t_id copyFiles( v_paths& from
-                 , v_paths& to
+    t_id deleteFiles( fs::paths& file, fs::copy_options options = fs::copy_options::none  );
+    t_id copyFiles( fs::paths& from
+                 , fs::paths& to
                  , fs::copy_options options = fs::copy_options::none );
-    t_id copyFiles( v_paths& from
+    t_id copyFiles( fs::paths& from
                  , const fs::path &to
                  , fs::copy_options options = fs::copy_options::none );
     t_id copyFile( const fs::path &from
                  , const fs::path &to
                  , fs::copy_options options = fs::copy_options::none );
-    t_id moveFiles( v_paths& from
-                 , v_paths& to
+    t_id moveFiles( fs::paths& from
+                 , fs::paths& to
                  , fs::copy_options options = fs::copy_options::none );
-    t_id moveFiles( v_paths& from
+    t_id moveFiles( fs::paths& from
                  , const fs::path &to
                  , fs::copy_options options = fs::copy_options::none );
     t_id moveFile( const fs::path &from
@@ -90,13 +93,13 @@ class FileManager
     static FileManager* currentInstance;
 
 
-    static void static_DeleteFiles( v_paths file, fs::copy_options options );
-    static void static_CopyFiles( v_paths from, v_paths to, fs::copy_options options );
-    static void static_MoveFiles( v_paths from, v_paths to, fs::copy_options options );
+    static void static_DeleteFiles( fs::paths file, fs::copy_options options );
+    static void static_CopyFiles( fs::paths from, fs::paths to, fs::copy_options options );
+    static void static_MoveFiles( fs::paths from, fs::paths to, fs::copy_options options );
 
-    void doDeleteFiles( v_paths file, fs::copy_options options );
-    void doCopyFiles( v_paths from, v_paths to, fs::copy_options options );
-    void doMoveFiles( v_paths from, v_paths to, fs::copy_options options );
+    void doDeleteFiles( fs::paths file, fs::copy_options options );
+    void doCopyFiles( fs::paths from, fs::paths to, fs::copy_options options );
+    void doMoveFiles( fs::paths from, fs::paths to, fs::copy_options options );
 
 
     HandleErrorCommand checkError( std::error_code& ec, FileSystemAction act, const fs::path p1 = fs::path(), const fs::path p2 = fs::path() );

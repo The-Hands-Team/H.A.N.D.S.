de4bc10764578fe9db9e3fe78e3772044e32419a
Parker <prkriley@gmail.com>
2016-03-25 19:29:20 -0500

    TestDir standardization
    Added test_setup.sh, which will set up your tests/test_dir directory. Removed the old tarball

---
+++ test_setup.sh
@@ -0,0 +1,11 @@
+rm -r tests/test_dir
+mkdir tests/test_dir
+touch tests/test_dir/file1.txt
+touch tests/test_dir/file2.txt
+mkdir tests/test_dir/dir1
+mkdir tests/test_dir/dir2
+touch tests/test_dir/dir1/sdfile1_1.txt
+touch tests/test_dir/dir1/sdfile1_2.txt
+mkdir tests/test_dir/dir1/sdir1
+touch tests/test_dir/dir1/sdir1/ssdfile1_1_1.txt
+touch tests/test_dir/dir2/sdfile2_1.txt

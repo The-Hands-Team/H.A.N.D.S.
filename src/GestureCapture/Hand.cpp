0f2cf3d0b18c6d0408d253f38523e5aa0207f6d1
Jordan Miller <jollyJordan@users.noreply.github.com>
2016-04-11 15:20:30 -0500

    All finger joint positions exposed

--- src/GestureCapture/Hand.cpp
+++ src/GestureCapture/Hand.cpp
@@ -19,11 +19,23 @@ Hand::Hand(Leap::Hand hand){
   palm[1] = palmPos.y;
   palm[2] = palmPos.z;
   
+  int f = 0;
   Leap::FingerList allTheFingers = hand.frame().fingers();
   for(Leap::FingerList::const_iterator fl=allTheFingers.begin(); fl!=allTheFingers.end(); fl++)
   {
     Leap::Vector fingerPos = (*fl).tipPosition();
     fingers.push_back( std::array<float, 3>{fingerPos.x, fingerPos.y,  fingerPos.z} );
+    
+    Leap::Bone bone;
+    Leap::Bone::Type boneType;
+    for(int b=0; b<4; b++)
+    {
+      boneType = static_cast<Leap::Bone::Type>(b);
+      bone = (*fl).bone(boneType);
+      Leap::Vector boneEnd = bone.nextJoint();
+      wow[f][b] = std::make_tuple(boneEnd.x, boneEnd.y, boneEnd.z);
+    }
+    f++;
   }
 }
 
@@ -45,6 +57,19 @@ std::tuple<float,float,float> Hand::getPalmLocation(){
 
 std::tuple<float,float,float> Hand::getFingerLocation(int fingerIndex){
   return std::make_tuple(fingers[fingerIndex][0]/box[0], fingers[fingerIndex][1]/box[1], fingers[fingerIndex][2]/box[2]);
+}
+
+
+std::array<std::array<std::tuple<float,float,float>, 4>, 5> Hand::getFingers(){
+  std::array<std::array<std::tuple<float,float,float>, 4>, 5> fings;
+  for(int f=0; f<5; f++)
+    for(int b=0; b<4; b++)
+    {
+    	float x,y,z;
+    	std::tie(x,y,z) = wow[f][b];
+      fings[f][b] = std::make_tuple(x/box[0], y/box[1], z/box[2]);
+    }
+  return std::move(fings);
 }
 
 Hand::~Hand(){

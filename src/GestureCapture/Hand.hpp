4d3c4c09c7ad93315ba2d3f06cf91b2f2d41a31e
Jordan Miller <jollyJordan@users.noreply.github.com>
2016-04-11 15:19:53 -0500

    All finger joint positions exposed

--- src/GestureCapture/Hand.hpp
+++ src/GestureCapture/Hand.hpp
@@ -13,6 +13,7 @@ private:
   float* box;
   float* palm;
   std::vector<std::array<float, 3> > fingers;
+  std::array<std::array<std::tuple<float,float,float>, 4>, 5> wow;
 
 public:
   std::vector<DirObject> objects;
@@ -23,6 +24,7 @@ public:
   bool isLeft();
   std::tuple<float,float,float> getPalmLocation();
   std::tuple<float,float,float> getFingerLocation(int fingerIndex);
+  std::array<std::array<std::tuple<float,float,float>, 4>, 5> getFingers();
 };
 
 #endif

8e28d2675208f3f6266a4de0bac94a36a3016af7
Jordan Miller <jollyJordan@users.noreply.github.com>
2016-04-11 09:13:02 -0500

    Stores hand attr, not Hand obj now

--- src/GestureCapture/Hand.cpp
+++ src/GestureCapture/Hand.cpp
@@ -1,32 +1,61 @@
 #include "Hand.hpp"
 
-Hand::Hand(Leap::Hand newHand){
-  hand = newHand;
+Hand::Hand(Leap::Hand hand){
+  if(!hand.isValid())
+    return;
+	
+  isRightHand = hand.isRight();
+  handID = hand.id();
+  
+  Leap::InteractionBox iBox = hand.frame().interactionBox();
+  box = new float[3];
+  box[0] = iBox.width();
+  box[1] = iBox.height();
+  box[2] = iBox.depth();
+  
+  Leap::Vector palmPos = hand.palmPosition();
+  palm = new float[3];
+  palm[0] = palmPos.x;
+  palm[1] = palmPos.y;
+  palm[2] = palmPos.z;
+  
+  int i = 0;
+  Leap::FingerList allTheFingers = hand.frame().fingers();
+  for(Leap::FingerList::const_iterator fl=allTheFingers.begin(); fl!=allTheFingers.end(); fl++)
+  {
+    Leap::Vector fingerPos = (*fl).tipPosition();
+	  fingers[i][0] = fingerPos.x;
+    fingers[i][1] = fingerPos.y;
+    fingers[i][2] = fingerPos.z;
+	  i++;
+  }
 }
 
 int Hand::getHandID(){
-  return hand.id();
+  return handID;
 }
 
 bool Hand::isRight(){
-  return hand.isRight();
+  return isRightHand;
 }
 
 bool Hand::isLeft(){
-  return hand.isLeft();
+  return !isRightHand;
 }
 
 std::tuple<float,float,float> Hand::getPalmLocation(){
-  Leap::Vector palmPos = hand.palmPosition();
-  Leap::InteractionBox iBox = hand.frame().interactionBox();
-  return std::make_tuple(palmPos.x/iBox.width(), palmPos.y/iBox.height(), palmPos.z/iBox.depth());
+  return std::make_tuple(palm[0]/box[0], palm[1]/box[1], palm[2]/box[2]);
 }
 
 std::tuple<float,float,float> Hand::getFingerLocation(int fingerIndex){
-  Leap::FingerList fingers = hand.fingers();
   if(fingerIndex>fingers.count())
     return NULL;
-  Leap::Vector fingerPos = fingers[fingerIndex].tipPosition();
-  Leap::InteractionBox iBox = hand.frame().interactionBox();
-  return std::make_tuple(fingerPos.x/iBox.width(), fingerPos.y/iBox.height(), fingerPos.z/iBox.depth());
+  return std::make_tuple(fingers[fingerIndex][0]/box[0], fingers[fingerIndex][1]/box[1], fingers[fingerIndex][2]/box[2]);
+}
+
+Hands::~Hands(){
+  if(box)
+    delete[] box;
+  if(palm)
+    delete[] palm;
 }
